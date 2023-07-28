074bfb1a6fc49b4af27f24a90f4eb82d59210ce0
easycodescript <68246535+easycodescript@users.noreply.github.com>
2023-07-28 13:11:59 +0200

    little style changes and others

--- weather-testcode-vue/src/components/TheWelcome.vue
+++ weather-testcode-vue/src/components/TheWelcome.vue
@@ -5,10 +5,34 @@ import ToolingIcon from './icons/IconTooling.vue'
 import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
-import { ref, computed } from 'vue'
+import { ref, computed, watchEffect } from 'vue'
 
 let inputValue = ref<string>('')
 let showSpinner = ref<boolean>(false)
+
+interface jsonResponse {
+  name: string
+  main?: {
+    temp: number
+    feels_like: number
+    temp_min: number
+    temp_max: number
+    pressure: number
+    humidity: number
+  }
+  weather?: Array<{
+    main: string
+    description: string
+    icon: string
+  }>
+}
+
+const API_URL =
+  'https://api.openweathermap.org/data/2.5/weather?q={location}&units=metric&appid=042559fc8f13bd0e86e557aa02965a24'
+
+let responseApi: jsonResponse = { name: '' }
+
+const location = computed(() => inputValue.value)
 
 function searchWeather(e: Event) {
   showSpinner.value = false
@@ -17,14 +41,19 @@ function searchWeather(e: Event) {
 
 function fetchWeather(value: string) {
   showSpinner.value = true
-  /* TODO: get data from Rest api */
+
+  watchEffect(async () => {
+    const url = API_URL.replace('{location}', location.value)
+    responseApi = await (await fetch(url)).json()
+    showSpinner.value = false
+  })
 }
 
 function resetForm() {
-  inputValue.value = ""
+  inputValue.value = ''
   showSpinner.value = false
+  responseApi = { name: '' }
 }
-
 </script>
 
 <template>
@@ -41,24 +70,33 @@ function resetForm() {
             @keyup="searchWeather($event)"
           />
           <div class="search-icon-wrapper">
-            <div id="search-icon" :style="{ 'display': !showSpinner ? '' : 'none' }">🔍</div>
-            <img src="../assets/spinner.gif" class="spinner-icon" :style="{ 'display': showSpinner ? '' : 'none' }"/>
+            <div id="search-icon" :style="{ display: !showSpinner ? '' : 'none' }">🔍</div>
+            <img
+              src="../assets/spinner.gif"
+              class="spinner-icon"
+              :style="{ display: showSpinner ? '' : 'none' }"
+            />
           </div>
         </div>
         <div class="search-history">history</div>
       </header>
       <article id="weather" class="weather">
-        <div class="icon-container">
-          <img id="weather__icon" src="" hidden />
+        <div class="icon-container" v-if="responseApi.weather">
+          <img
+            id="weather__icon"
+            :src="`http://openweathermap.org/img/wn/${responseApi.weather[0].icon}@4x.png`"
+          />
+        </div>
+        <div v-if="responseApi.weather && responseApi.main">
+          <h2 id="weather__main">
+            {{ responseApi.weather[0].main + ', ' + responseApi.main.temp + '°' }}
+          </h2>
         </div>
         <div>
-          <h2 id="weather__main"></h2>
+          <h2 id="weather__city">{{ responseApi.name }}</h2>
         </div>
-        <div>
-          <h2 id="weather__city"></h2>
-        </div>
-        <div>
-          <p id="weather__description"></p>
+        <div v-if="responseApi.weather">
+          <p id="weather__description">{{ responseApi.weather[0].description }}</p>
         </div>
       </article>
     </main>
@@ -90,6 +128,7 @@ function resetForm() {
   justify-content: center;
   align-items: center;
   margin-top: 1rem;
+  margin-bottom: 3rem;
   flex-direction: column;
 }
 
@@ -111,7 +150,8 @@ function resetForm() {
   box-shadow: var(--pink) 0 0.2rem;
 }
 
-.search-icon-wrapper, .spinner-icon {
+.search-icon-wrapper,
+.spinner-icon {
   position: absolute;
   top: 50%;
   transform: translateY(-50%);
@@ -135,6 +175,7 @@ function resetForm() {
   flex-direction: column;
   align-items: center;
   margin-bottom: 5rem;
+  color: var(--vt-c-black);
 }
 
 .icon-container {
